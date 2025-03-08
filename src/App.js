b735c04e3b718dc74827324500f08079ad2cb0fa
Dtsvetaev <dtsvetaev@yahoo.com>
2025-03-08 09:38:20 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Portfolio from "./components/Portfolio";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Портфолио с фильтрами</h1>
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
